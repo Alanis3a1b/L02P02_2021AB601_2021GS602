36b2f0886292dccb2d61e01b382e7499cf605384
ALANIS ALESSANDRA ALVAREZ BAÑOS <2021AB601@unicaeselsalvador0.onmicrosoft.com>
2024-04-20 09:10:49 -0600

    Se creo controlador y vista de librerías

---
+++ L02P02_2021AB601_2021GS602/Controllers/LibreriaController.cs
@@ -0,0 +1,14 @@
+﻿using L02P02_2021AB601_2021GS602.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L02P02_2021AB601_2021GS602.Controllers
+{
+    public class LibreriaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
