2761a656175651a6a26f7e9c63af3d321ac65947
ALANIS ALESSANDRA ALVAREZ BAÑOS <2021AB601@unicaeselsalvador0.onmicrosoft.com>
2024-04-20 08:48:04 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2021AB601_2021GS602/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2021AB601_2021GS602.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
